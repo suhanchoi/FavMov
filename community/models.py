dff1ee22e4b1156ff5e37e6c668832eb8865cc35
haru <jiaiha@naver.com>
2021-11-18 17:34:22 +0900

    별 수정

--- community/models.py
+++ community/models.py
@@ -5,12 +5,22 @@ from django.conf import settings
 class Review(models.Model):
     title = models.CharField(max_length=100)
     movie_title = models.CharField(max_length=50)
-    rank = models.IntegerField()
+    RANKS = [
+    (1, '★'),
+    (2, '★★'),
+    (3, '★★★'),
+    (4, '★★★★'),
+    (5, '★★★★★'),
+    ]
+    rank = models.IntegerField(choices=RANKS, default=5)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_reviews')
+  
+    
+
 
 
 class Comment(models.Model):
