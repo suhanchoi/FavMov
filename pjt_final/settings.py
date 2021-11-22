b75be77c575601ba79e0bbc4ff1f28f4ef89c7bb
suhan <suhan9603@naver.com>
2021-11-23 02:08:22 +0900

    settings.py

--- pjt_final/settings.py
+++ pjt_final/settings.py
@@ -120,7 +120,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
+import os 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 AUTH_USER_MODEL = 'accounts.User'
 
