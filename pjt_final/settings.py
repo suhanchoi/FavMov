090d9d58c023b6a94703a9d7b16cfc67a25c424e
suhan <suhan9603@naver.com>
2021-11-23 13:38:30 +0900

    settings.py

--- pjt_final/settings.py
+++ pjt_final/settings.py
@@ -128,12 +128,16 @@ AUTH_USER_MODEL = 'accounts.User'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-DEBUG = True
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-# import os 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# DEBUG = False
-# ALLOWED_HOSTS = [ 'favmov.pythonanywhere.com' ]
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_URL = '/static/'
+
+
+# DEBUG = True
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+import os 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+DEBUG = False
+ALLOWED_HOSTS = [ 'favmov.pythonanywhere.com' ]
