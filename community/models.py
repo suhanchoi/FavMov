573bc1288b5e653bb9fbd7e434b0f3f24d22610d
suhan <suhan9603@naver.com>
2021-11-25 17:25:43 +0900

    기능 add

--- community/models.py
+++ community/models.py
@@ -32,3 +32,5 @@ class Comment(models.Model):
     content = models.TextField()
     review = models.ForeignKey(Review, on_delete=models.CASCADE)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    review_comment_created_at = models.DateTimeField(auto_now_add=True)
+    review_comment_updated_at = models.DateTimeField(auto_now=True)
